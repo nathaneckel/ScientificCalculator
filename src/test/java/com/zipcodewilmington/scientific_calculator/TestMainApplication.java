214998cffc4b06a7c4fdc1e32f946bcb88547bf6
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-02 12:46:47 -0500

    added log test method that doesn't work yet

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,15 @@
 package com.zipcodewilmington.scientific_calculator;
+import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
+        @Test
+        public void testLog() throws Exception {
+            Assert.assertEquals(TestMainApplication.log(1));
+        }
+        
 }
