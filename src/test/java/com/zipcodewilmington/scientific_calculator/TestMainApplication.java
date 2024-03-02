1172eab5149e3710b6cc9ea61a91c0dbbc5d5fd5
Abukarabukare <Abukarshegow@gmail.comm>
2024-03-02 12:52:10 -0500

    I added

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,18 @@
 package com.zipcodewilmington.scientific_calculator;
+import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+
+        @Test
+        public void testLog() throws Exception {
+            Assert.assertEquals(TestMainApplication.log(1));
+        }
+        
 }
+
+
+//I amd adding things
