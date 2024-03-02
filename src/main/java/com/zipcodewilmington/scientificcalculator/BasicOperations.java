9454dbf4821fb64493de5a2fd2578d82b269ec3e
nathaneckel <nathan@patientpaperwork.com>
2024-03-02 15:39:17 -0500

    made a few basic operations

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicOperations.java
@@ -0,0 +1,80 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class BasicOperations {
+
+    //Nick's example that I'm customizing - TY Nick!
+
+    //HERE's WHAT [UPDATE: NICK & I] NEED TO DO:
+    /*
+     DEFAULT 0 DISPLAY STATE.
+     GET/DISPLAY CURRENT NUMBER
+     CLEAR THE DISPLAY
+     CHANGE THE NUMBER ON THE DISPLAY
+     ADD SUBTRACT MULTIPLY AND DIVIDE THE DISPLAY VALUE BY A GIVEN NUMBER
+
+    I NEED TO DO THIS
+    ~~SIMPLE FUNCTIONS~~
+    2 SQUARE / SQUARE ROOT OF DISPLAY NUMBER
+    3 VARIABLE EXPONENTIATION (Xy)
+    4 INVERSE OF DISPLAY NUMBER (1/X)
+    5 INVERT THE SIGN OF NUMBER (TOGGLE +/-)
+    6 DISPLAY "ERROR" WHEN ERROR
+    7 CLEAR THE ERROR BEFORE YOU CAN DO ANYTHING ELSE
+
+    NATHAN CUSTOMIZE NICK'S CODE BELOW AND FIGURE THE CONTEXT WHERE IT FITS IN
+
+    */
+    // SIMPLE FUNCTIONS
+    public static double add(double userInput) {
+        return x + y;
+    }
+    public static double subtract(double userInput){
+        return x - y;
+    }
+    public static double multiply(double userInput){
+        return x * y;
+    }
+    public static double divide(double userInput){
+        return x / y;
+    }
+   /* SQUARE
+int i = 2;
+int square = Math.pow(i, 2);
+
+    */
+
+
+
+    //SQUARE ROOT
+    // Java program to demonstrate working
+// of java.lang.Math.sqrt() method
+
+    /*import java.lang.Math;
+
+    class Gfg {
+
+        // driver code
+        public static void main(String args[])
+        {
+            double a = 30;
+
+            System.out.println(Math.sqrt(a));
+
+            a = 45;*/
+
+    //VARIABLE EXPONENTIATION (Xy)
+    public static double exponent(double userInput){
+        return double Math.pow;(base, exponent);
+    }
+
+    //INVERSION / INVERSE OF NUMBER
+    /*How do you find the inverse of a number in Java?
+
+     */
+    public static double divide(double userInput) {
+        return x / y;
+    }
+    public static double divide(double userInput){
+        return x / y;
+    }
+}
